b4ff32304b2b2bdec7a650507cbb4917c05e2713
kibbha <dispigno.geneva@gmail.com>
2026-09-22 23:12:14 +0200

    test: verify POS Academy sandbox i18n permissions and offline cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='remapro-pos-shell-v0270';
+const CACHE='remapro-pos-shell-v0270-academy-final';
 const CORE=['./','./index.html','./manifest.webmanifest','./runtime-config.js','./src/app.js','./src/cloud.js','./src/db.js','./src/printer.js','./src/layout.js','./src/academy-content.js','./src/academy.js','./src/styles.css','./assets/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
