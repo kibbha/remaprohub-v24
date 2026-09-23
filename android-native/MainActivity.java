40a890ee7435d34b2aa0b3fb9f71441cf845584a
kibbha <dispigno.geneva@gmail.com>
2026-09-23 21:31:35 +0200

    fix(pos): use public onResume for BridgeActivity

--- android-native/MainActivity.java
+++ android-native/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         hideSystemBars();
     }
